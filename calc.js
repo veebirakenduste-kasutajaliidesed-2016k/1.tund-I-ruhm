15aa438b8cab7046d19965a4f7bc1b18516cdc67
romil rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-02-01 12:16:31 +0200

    to inch convertion

--- calc.js
+++ calc.js
@@ -1,1 +1,21 @@
-// calc.js
+//käivitub siis kui terve dokument laetud
+window.onload = function(){
+  console.log('laetud');
+  // saan kätte input väljad
+
+  var cm = document.getElementById('cm');
+  var inch = document.getElementById('inch');
+
+  // lisan nuppudele kuularid
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+
+  //console.log(inch);
+};
+
+
+function convertToInch(){
+  console.log('vajutas nuppu');
+  //  arvutan ümber tollideks ja lisan väärtuse tollid lahtrisse
+  inch.value = cm.value/2.54;
+
+}
